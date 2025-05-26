24c65f0925462bd46e3b89b4742165d604dbd16f
23R11A0529 <23r11a0529@gcet.edu.in>
2025-05-26 20:27:13 +0530

    updated

--- src/components/App14.jsx
+++ src/components/App14.jsx
@@ -11,13 +11,35 @@ export default function App14() {
   useEffect(() => {
     fetchData();
   }, []);
+  const [txt, setTxt] = useState();
+  const [filtered, setFiltered] = useState([]);
+  const handleSearch = () => {
+   const User = users.filter((user) =>
+      user.name.toLowerCase().includes(txt.toLowerCase())
+    );
+    setFiltered(User);
+  };
   return (
     <div>
-      <h1>App14</h1>
+      <h1 style={{ color: "#d86c7a" }}>App14</h1>
       <h2>useEffect hook to fetch api</h2>
-      <p>
-        <input type="text" placeholder="Search name" />
+       
+        <p>
+        <input
+          type="text"
+          placeholder="Search Name"
+          onChange={(e) => setTxt(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
       </p>
+      <ol>
+        {filtered.map((value) => (
+          <li key={value.name}>
+            {value.name}, {value.email}, {value.phone}
+          </li>
+        ))}
+      </ol>
+      <hr />
       <ol>
         {users &&
           users.map((value) => (
